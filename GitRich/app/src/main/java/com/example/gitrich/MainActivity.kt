f69a0a54517f1869ccf8a9a316629960b5382f1d
Ong Jun Xiang <ongjx9@hotmail.com>
2021-03-06 17:17:38 +0800

    Create QR Scanning Page

--- GitRich/app/src/main/java/com/example/gitrich/MainActivity.kt
+++ GitRich/app/src/main/java/com/example/gitrich/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gitrich
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchQR(view: View) {
+        val intent = Intent(this, QRScannerActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ GitRich/app/src/main/java/com/example/gitrich/QRScannerActivity.kt
@@ -0,0 +1,97 @@
+package com.example.gitrich
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+
+private const val CAMERA_REQUEST_CODE = 101
+
+class QRScannerActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_q_r_scanner)
+        setupPermissions()
+
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        val tv_textView = findViewById<TextView>(R.id.tv_textView)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.CONTINUOUS // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                tv_textView.text = it.text
+                val url = Uri.parse(it.text)
+//                val intent = Intent(Intent.ACTION_VIEW, url)
+                val intent = Intent(this, QRScannerResultActivity::class.java)
+                intent.putExtra("url", it.text)
+                startActivity(intent)
+//
+//                val intent = Intent(Intent.ACTION_VIEW)
+//                intent.setDataAndType(url, "application/pdf")
+//                intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+//                startActivity(intent)
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(this, "Camera initialization error: ${it.message}",
+                        Toast.LENGTH_LONG).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    // avoid memory leaks
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You need camera permission to use this app", Toast.LENGTH_SHORT)
+                }
+            }
+        }
+    }
+}
