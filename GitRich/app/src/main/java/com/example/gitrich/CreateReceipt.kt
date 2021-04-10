90da81f97925476f6408b9a96de741433f256b9e
CHANG Ern Rae <erchang.2017@smu.edu.sg>
2021-04-10 16:52:23 +0800

    uncomment save

--- GitRich/app/src/main/java/com/example/gitrich/CreateReceipt.kt
+++ GitRich/app/src/main/java/com/example/gitrich/CreateReceipt.kt
@@ -91,75 +91,75 @@ class CreateReceipt : AppCompatActivity() {
         }
     }
 
-//    fun save(view: View) {
-//
-//        amount = findViewById<EditText>(R.id.create_amount).text.toString()
-//        date = findViewById<EditText>(R.id.create_date).text.toString()
-//        store = findViewById<EditText>(R.id.create_store).text.toString()
-//        desc = findViewById<EditText>(R.id.create_desc).text.toString()
-//
-//        var itemsObject = JSONObject()
-//
-//        if (desc.equals("")) {
-//            // do nothing
-//        } else {
-//            try {
-//                val items = desc.splitToSequence("\n")
-//
-//                for (item: String in items) {
-//                    val l = item.split(',')
-//                    val name = l[0].trim()
-//                    val amount = if ("$" in l[1]) l[1].trim() else ("$${l[1].trim()}")
-//
-//                    itemsObject.put(l[0].trim() as String, l[1])
-//                }
-//            } catch (e: Exception) {
-//                // We can choose to stop processing unless its empty or appropriate desc is entered
-//                println("Inappropriate Description")
-//                itemsObject = JSONObject()
-//            }
-//        }
-//
-//
-//        val dateRegex = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$"
-//        if (!Pattern.matches(dateRegex, date)){
-//            Toast.makeText(this, "Invalid date format", Toast.LENGTH_SHORT).show()
-//        }
-//        else if (amount == "" || date == "" || store == "") {
-//            Toast.makeText(this, "Please ensure that there are no empty fields!", Toast.LENGTH_SHORT).show()
-//        }
-//        else {
-//            val client = OkHttpClient();
-//            val jsonObject = JSONObject()
-//
-//            jsonObject.put("name", store)
-//            jsonObject.put("amount", amount)
-//            jsonObject.put("items", itemsObject)
-//            jsonObject.put("image", "")
-//            jsonObject.put("date", date)
-//            jsonObject.put("category", category)
-//
-//            val user = MySingleton.getUsername()
-//            val url = "http://ec2-18-136-119-32.ap-southeast-1.compute.amazonaws.com:8000/users/${user}/qr-receipts"
-//            val jsonObjectRequest = JsonObjectRequest(
-//                    Request.Method.POST, url, jsonObject,
-//                    { response ->
-//                        val res = response.getInt("code")
-//                        Log.e("response", response.toString())
-//                        if (res == 201){
-//                            val goBack = Intent()
-//                            setResult(RESULT_OK)
-//                            finish()
-//                        }
-//                    },
-//                    { error ->
-//                        Log.e("Error", error.toString())
-//                    }
-//            )
-//            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
-//        }
-//
-//
-//    }
+    fun save(view: View) {
+
+        amount = findViewById<EditText>(R.id.create_amount).text.toString()
+        date = findViewById<EditText>(R.id.create_date).text.toString()
+        store = findViewById<EditText>(R.id.create_store).text.toString()
+        desc = findViewById<EditText>(R.id.create_desc).text.toString()
+
+        var itemsObject = JSONObject()
+
+        if (desc.equals("")) {
+            // do nothing
+        } else {
+            try {
+                val items = desc.splitToSequence("\n")
+
+                for (item: String in items) {
+                    val l = item.split(',')
+                    val name = l[0].trim()
+                    val amount = if ("$" in l[1]) l[1].trim() else ("$${l[1].trim()}")
+
+                    itemsObject.put(l[0].trim() as String, l[1])
+                }
+            } catch (e: Exception) {
+                // We can choose to stop processing unless its empty or appropriate desc is entered
+                println("Inappropriate Description")
+                itemsObject = JSONObject()
+            }
+        }
+
+
+        val dateRegex = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$"
+        if (!Pattern.matches(dateRegex, date)){
+            Toast.makeText(this, "Invalid date format", Toast.LENGTH_SHORT).show()
+        }
+        else if (amount == "" || date == "" || store == "") {
+            Toast.makeText(this, "Please ensure that there are no empty fields!", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            val client = OkHttpClient();
+            val jsonObject = JSONObject()
+
+            jsonObject.put("name", store)
+            jsonObject.put("amount", amount)
+            jsonObject.put("items", itemsObject)
+            jsonObject.put("image", "")
+            jsonObject.put("date", date)
+            jsonObject.put("category", category)
+
+            val user = MySingleton.getUsername()
+            val url = "http://ec2-18-136-119-32.ap-southeast-1.compute.amazonaws.com:8000/users/${user}/qr-receipts"
+            val jsonObjectRequest = JsonObjectRequest(
+                    Request.Method.POST, url, jsonObject,
+                    { response ->
+                        val res = response.getInt("code")
+                        Log.e("response", response.toString())
+                        if (res == 201){
+                            val goBack = Intent()
+                            setResult(RESULT_OK)
+                            finish()
+                        }
+                    },
+                    { error ->
+                        Log.e("Error", error.toString())
+                    }
+            )
+            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+        }
+
+
+    }
 }
 
